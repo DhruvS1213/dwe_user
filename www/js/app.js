a5a1b6182e69d78e39cceda8ea21308f577f7ba3
Dhruv Shah <s.dhruv@tcs.com>
2016-11-02 18:46:55 +0530

    resolved pager click issue

--- www/js/app.js
+++ www/js/app.js
@@ -47,11 +47,14 @@ angular.module('dweUser', ['ionic'])
     });
 
     vm.closeModal = function() {
-    console.log('closemodal function');
-    $(video).remove()
-      $scope.modal.hide();
+        console.log('closemodal function');
+        $scope.modal.hide();
     };
 
+    vm.slide = function(index) {
+        console.log('slide pager');
+        $ionicSlideBoxDelegate.slide(index);
+    };
     vm.goToSlide = function(index) {
         console.log('goto slide function')
       $scope.modal.show();
@@ -79,7 +82,7 @@ angular.module('dweUser', ['ionic'])
     vm.myInterval = 3000;
     
 
-     vm.data.imgArray = ['/img/img-2.jpg','/img/img-3.jpg','/img/img-4.jpg','/img/img-5.jpg']
+     vm.data.imgArray = ['img/img-2.jpg','img/img-3.jpg','img/img-4.jpg','img/img-5.jpg']
      vm.imageDescription = ['Personally, after having a car for several years, I’ve gone without for the past several years that I’ve lived in Stockholm. And whenever we thought about getting one, the counterpoint always was ‘What’s the point? We only need it for maybe one day a month tops, and having it here in the city will end up costing us money.’ But with the 01, we could lend it to friends without a hassle, or even try to balance the cost of it out by being part of the car-sharing solution.', 'Christine helps drive the social media activation of the Networked Society and believes in all things connected. A communicator and storyteller by trade, she has been excited about the future since she was a kid—and is still waiting for her self-driving flying car. Follow Christine on Twitter @christineluby', 'As 4G LTE and smartphones have penetrated the developed markets, the world has started to prepare for 5G. So what are the five most important innovations we can expect with 5G?', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'];
 
     vm.videoPath.push($sce.trustAsResourceUrl('//www.kaltura.com/p/1614541/sp/0/playManifest/entryId/1_vc8f3h2d/format/url/flavorParamId/487111/video'));
