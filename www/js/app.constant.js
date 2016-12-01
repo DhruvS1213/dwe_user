d9e3ee7ba48ad0815cd74c9dba9f04357153db26
DhruvS1213 <dhruv.shah4192@gmail.com>
2016-12-01 11:46:51 +0530

    Changed url for fetching data

--- www/js/app.constant.js
+++ www/js/app.constant.js
@@ -2,5 +2,5 @@ angular.module('dweUser')
         .constant('appConstants', {
             demoId : 1,
             timerValue : 5000,
-            url : 'http://localhost:9000'
+            url : 'http://104.41.211.220:9000'
         });
