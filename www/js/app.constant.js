6064de29a9aed48962c1e3e6186871683fc4a604
Harsh Patel <patelharsh867@gmail.com>
2016-12-01 12:59:41 +0530

    Nexus css added

--- www/js/app.constant.js
+++ www/js/app.constant.js
@@ -1,6 +1,6 @@
 angular.module('dweUser')
         .constant('appConstants', {
             demoId : 1,
-            timerValue : 5000,
+            timerValue : 500000000,
             url : 'http://104.41.211.220:9000'
         });
