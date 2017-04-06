4cee3f9999f4daccd3d74e60e0560d1f6c23fcd7
DhruvS1213 <dhruv.shah4192@gmail.com>
2017-04-06 19:42:10 +0530

    updated url

--- www/js/app.constant.js
+++ www/js/app.constant.js
@@ -2,5 +2,5 @@ angular.module('dweUser')
         .constant('appConstants', {
             demoId : 1,
             timerValue : 10000,
-            url : 'http://10.29.24.243:9000'
+            url : 'http://137.116.170.146:9000'
         });
