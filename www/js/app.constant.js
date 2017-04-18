436121ec5358205b7c4af89adc1858e6c459b860
Harsh Patel <patelharsh867@gmail.com>
2017-04-18 22:31:52 +0530

    Changed ip in app constant file

--- www/js/app.constant.js
+++ www/js/app.constant.js
@@ -2,5 +2,5 @@ angular.module('dweUser')
         .constant('appConstants', {
             demoId : 1,
             timerValue : 120000,
-            url : 'http://137.116.170.146:9000'
+            url : 'http://13.79.35.232:9000'
         });
